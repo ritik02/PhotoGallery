526e8c348d39f59ee8b32f0027518c123d8b056e
Ritik Verma <ritikverma@Ritiks-MacBook-Pro.local>
2018-05-23 12:29:10 +0530

    Add Category Model Validation

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,10 @@ class CategoryTest < ActiveSupport::TestCase
   test "the truth" do
    assert true
   end
+
+  test "should be invalid without Category name" do
+    categories(:one).name = nil
+    assert_nil categories(:one).name
+    assert_equal false, categories(:one).valid?
+  end
 end
